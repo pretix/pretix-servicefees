f6e8e95e9610f10fc7237c8f1cceece2c03b54b1
Raphael Michel <mail@raphaelmichel.de>
2018-09-11 18:01:59 +0200

    Release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-servicefees',
-    version='1.0.1',
+    version='1.0.2',
     description='Allows you to impose a service fee on all non-free orders.',
     long_description=long_description,
     url='https://github.com/pretix/pretix-servicefees',
