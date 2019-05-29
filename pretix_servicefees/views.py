cb1b5a07c74648ced68a21818f4aa069b98a9575
Raphael Michel <mail@raphaelmichel.de>
2019-05-29 09:27:24 +0200

    Make all settings fields optional

--- pretix_servicefees/views.py
+++ pretix_servicefees/views.py
@@ -9,27 +9,37 @@ from pretix.helpers.money import change_decimal_field
 
 
 class ServiceFeeSettingsForm(SettingsForm):
-    service_fee_abs = forms.DecimalField(label=_('Fixed fee per order'))
+    service_fee_abs = forms.DecimalField(
+        label=_('Fixed fee per order'),
+        required=False
+    )
     service_fee_percent = forms.DecimalField(
         label=_('Percentual fee per order'),
         help_text=_('Percentage of the order total. Note that this percentage will currently only '
                     'be calculated on the summed price of sold tickets, not on other fees like e.'
-                    'g. shipping fees, if there are any.')
+                    'g. shipping fees, if there are any.'),
+        required=False
     )
     service_fee_per_ticket = forms.DecimalField(
         label=_('Fixed fee per ticket'),
-        help_text=_('This fee will be added for each ticket sold, except for free items and addons.')
+        help_text=_('This fee will be added for each ticket sold, except for free items and addons.'),
+        required=False
     )
 
-    service_fee_abs_resellers = forms.DecimalField(label=_('Fixed fee per order'))
+    service_fee_abs_resellers = forms.DecimalField(
+        label=_('Fixed fee per order'),
+        required=False
+    )
     service_fee_percent_resellers = forms.DecimalField(
         label=_('Percentual fee per order'),
         help_text=_('Percentage of the order total. Note that this percentage will currently only '
                     'be calculated on the summed price of sold tickets, not on other fees like e.'
-                    'g. shipping fees, if there are any.')
+                    'g. shipping fees, if there are any.'),
+        required=False
     )
     service_fee_per_ticket_resellers = forms.DecimalField(
         label=_('Fixed fee per ticket'),
+        required=False,
         help_text=_('This fee will be added for each ticket sold, except for free items and addons.')
     )
 
