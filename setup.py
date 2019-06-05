f5bb9794e07f8a8d7f196211229bd21af5fbe0a6
Raphael Michel <mail@raphaelmichel.de>
2019-06-05 15:56:27 +0200

    Bump to 1.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-servicefees',
-    version='1.2.0',
+    version='1.3.0',
     description='Allows you to impose a service fee on all non-free orders.',
     long_description=long_description,
     url='https://github.com/pretix/pretix-servicefees',
